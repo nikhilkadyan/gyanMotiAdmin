5051e2a0c38aba05c05416358084f81d807e62f9
nikhilkadyan <nikhilkadyan@outlook.in>
2020-06-03 23:26:54 +0530

    1 file modified

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../core/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  expand: boolean = false;
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  toggleNavbar(){
+    this.expand = !this.expand;
   }
 
 }
